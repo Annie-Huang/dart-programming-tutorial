33a57c5b17aca137ddafa4c70dae38c4b5152b14
Annie Huang <annie.hn@gmail.com>
2020-01-10 20:31:55 +1100

    do{..} while (num > 0);

--- playground.dart
+++ playground.dart
@@ -345,10 +345,15 @@ void main() {
   // while loop
   int num = 5;
 
-  while (num > 0) {
+//  while (num > 0) {
+//    print(num);
+//    num -=1;
+//  }
+
+  do {
     print(num);
     num -=1;
-  }
+  } while (num > 0);
 }
 
 
