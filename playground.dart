4dfa933770c00582428de17511ccb94832c4a1f7
Annie Huang <annie.hn@gmail.com>
2020-01-10 17:24:06 +1100

    int  double  String  bool  dynamic

--- playground.dart
+++ playground.dart
@@ -17,11 +17,102 @@ import 'dart:io';
 
 // ======================================================
 
+//main() {
+//  stdout.writeln('What is your name?');
+//  String name = stdin.readLineSync();
+//  print('My name is $name');
+//}
+////What is your name?
+////annie huang
+////My name is annie huang
+
+// ======================================================
+
+// In-line comment
+
+/*
+Block comment
+multiple line of comment
+ */
+
+/// Documentation
+
+// ======================================================
+
+/*
+Strongly Typed Language: The type of a variable is known at compile time.
+For exmaple: C++, Java Swift
+
+Dynamic Typed Language: The type of a variable is known at run time
+For example: Python, Ruby, Javascript.
+ */
+
 main() {
-  stdout.writeln('What is your name?');
-  String name = stdin.readLineSync();
-  print('My name is $name');
+  /*
+  int
+  double
+  String
+  bool
+  dynamic
+   */
+
+  int amount1 = 100;
+  var amount2 = 200;
+  print('Amount1: $amount1 | Amount2: $amount2 \n');
+
+  double dAmount1 = 100.11;
+  var dAmount2 = 200.22;
+  print('dAmount1: $dAmount1 | dAmount2: $dAmount2 \n');
+
+  String name1 = 'Mahmud';
+  var name2 = 'Ahsan';
+
+  print('My name is $name1 $name2 \n');
+
+  bool isItTrue1 = true;
+  var isItTrue2 = false;
+  print('isItTrue1: $isItTrue1 | isItTrue2: $isItTrue2 \n');
+
+  dynamic weakVariable = 100;
+  print('weakVariable 1: $weakVariable \n');
+
+  weakVariable = 'Dart Programming';
+  print('weakVariable 2: $weakVariable \n');
+
+  weakVariable = null;
+  print(weakVariable);
 }
-//What is your name?
-//annie huang
-//My name is annie huang
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
