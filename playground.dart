397c0a41e5020c01febb2f0bd6eff5803cc870c4
Annie Huang <annie.hn@gmail.com>
2020-01-10 21:45:56 +1100

    Arrow function

--- playground.dart
+++ playground.dart
@@ -464,16 +464,18 @@ multiple line of comment
 
 // Function
 void main() {
+  // Arrow Function =>
   showOutput(square(2));
   showOutput(square(2.5));
 
   print(square.runtimeType);
 }
 
-// dynamic means it can return any type.
-dynamic square(var num) {
-  return num * num;
-}
+//// dynamic means it can return any type.
+//dynamic square(var num) {
+//  return num * num;
+//}
+dynamic square(var num) => num * num;
 
 void showOutput(var msg) {
   print(msg);
