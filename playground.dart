bf32168b995f0ba541a2d91dccf4fc85b9235ab1
Annie Huang <annie.hn@gmail.com>
2020-01-10 17:50:01 +1100

    int -> String | double -> String

--- playground.dart
+++ playground.dart
@@ -128,18 +128,29 @@ multiple line of comment
 
 // ======================================================
 
+//main() {
+//  // String -> int
+//  var one = int.parse('1');
+//  var one1 = int.parse('str');
+//  assert(one == 1);
+//
+//  // String -> double
+//  var onePointOne = double.parse('1.1');
+//  assert(onePointOne == 1.1);
+//}
+
+// ======================================================
+
 main() {
-  // String -> int
-  var one = int.parse('1');
-  var one1 = int.parse('str');
-  assert(one == 1);
+  // int -> String
+  String oneAsString = 1.toString();
+  assert(oneAsString == '1');
 
-  // String -> double
-  var onePointOne = double.parse('1.1');
-  assert(onePointOne == 1.1);
+  // double -> String
+  String piAsString = 3.14159.toStringAsFixed(2);
+  assert(piAsString == '3.14');
+
 }
-
-
 
 
 
