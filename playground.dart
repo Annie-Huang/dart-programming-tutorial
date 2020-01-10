86a21ab00b541bf3312373e1c1ceb606fdc10818
Annie Huang <annie.hn@gmail.com>
2020-01-10 18:01:22 +1100

    int num without assign to a value will set it to null, like Javascript, null is a special type.

--- playground.dart
+++ playground.dart
@@ -154,21 +154,27 @@ multiple line of comment
 
 // ======================================================
 
+//main() {
+//  const aConstNum = 0; // int constant
+//  const aConstBool = true; // bool constant
+//  const aConstString = 'a constant string'; // string constant
+//
+//  print(aConstNum);
+//  print(aConstBool);
+//  print(aConstString);
+//
+//  print(aConstNum.runtimeType);
+//  print(aConstBool.runtimeType);
+//  print(aConstString.runtimeType);
+//}
+
+// ======================================================
+
 main() {
-  const aConstNum = 0; // int constant
-  const aConstBool = true; // bool constant
-  const aConstString = 'a constant string'; // string constant
-
-  print(aConstNum);
-  print(aConstBool);
-  print(aConstString);
-
-  print(aConstNum.runtimeType);
-  print(aConstBool.runtimeType);
-  print(aConstString.runtimeType);
+//  int num;
+  int num = null;
+  print(num);
 }
-
-
 
 
 
