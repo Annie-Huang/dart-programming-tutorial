d86c4bd11dbb1a3f151ca235dd60cc6bedee7dda
Annie Huang <annie.hn@gmail.com>
2020-01-10 16:56:14 +1100

    stdout and stdin

--- playground.dart
+++ playground.dart
@@ -1,13 +1,27 @@
 // dart: core   << You don't have to import it. Usually dart import it automatically.
 //import 'dart:core';
+import 'dart:io';
 
 //main() {
 //  print('hello world');
 //}
 
-main() {
-    var firstName = 'Mahmud';
-    String lastName = 'Ahsan';
+// ======================================================
 
-    print(firstName + ' ' + lastName);
+//main() {
+//    var firstName = 'Mahmud';
+//    String lastName = 'Ahsan';
+//
+//    print(firstName + ' ' + lastName);
+//}
+
+// ======================================================
+
+main() {
+  stdout.writeln('What is your name?');
+  String name = stdin.readLineSync();
+  print('My name is $name');
 }
+//What is your name?
+//annie huang
+//My name is annie huang
