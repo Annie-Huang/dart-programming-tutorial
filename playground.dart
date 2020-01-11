f93527ac17df6f6a6dc3ec5735e2258d1ebd5a53
Annie Huang <annie.hn@gmail.com>
2020-01-11 18:20:53 +1100

    Named constructor

--- playground.dart
+++ playground.dart
@@ -537,6 +537,12 @@ class Person {
 //  }
   Person(this.name, [this.age = 18]);
 
+  // named constructor
+  Person.guest() {
+    name = 'Guest';
+    age = 18;
+  }
+
   void showOutput() {
     print(name);
     print(age);
@@ -550,6 +556,12 @@ void main() {
 //  person1.name = 'Mahmud';
 //  person1.age = 35;
   person1.showOutput();
+
+  var person2 = Person('Jack', 25);
+  person2.showOutput();
+
+  var person3 = Person.guest();
+  person3.showOutput();
 }
 
 
