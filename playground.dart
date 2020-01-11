060d5f822399d8729859d71188fd95fecea0e07e
Annie Huang <annie.hn@gmail.com>
2020-01-11 18:22:29 +1100

    commented out code

--- playground.dart
+++ playground.dart
@@ -525,45 +525,46 @@ multiple line of comment
 
 // ======================================================
 
-// Class
-
-class Person {
-  String name;
-  int age;
-
-//  Person(String name, [int age=18]) {
-//    this.name = name;
-//    this.age = age;
+//// Class
+//
+//class Person {
+//  String name;
+//  int age;
+//
+////  Person(String name, [int age=18]) {
+////    this.name = name;
+////    this.age = age;
+////  }
+//  Person(this.name, [this.age = 18]);
+//
+//  // named constructor
+//  Person.guest() {
+//    name = 'Guest';
+//    age = 18;
 //  }
-  Person(this.name, [this.age = 18]);
+//
+//  void showOutput() {
+//    print(name);
+//    print(age);
+//  }
+//}
+//
+//void main() {
+//  Person person1 = Person('Mahmud');
+////  Person person1 = Person('Mahmud', 35);
+//
+////  person1.name = 'Mahmud';
+////  person1.age = 35;
+//  person1.showOutput();
+//
+//  var person2 = Person('Jack', 25);
+//  person2.showOutput();
+//
+//  var person3 = Person.guest();
+//  person3.showOutput();
+//}
 
-  // named constructor
-  Person.guest() {
-    name = 'Guest';
-    age = 18;
-  }
-
-  void showOutput() {
-    print(name);
-    print(age);
-  }
-}
-
-void main() {
-  Person person1 = Person('Mahmud');
-//  Person person1 = Person('Mahmud', 35);
-
-//  person1.name = 'Mahmud';
-//  person1.age = 35;
-  person1.showOutput();
-
-  var person2 = Person('Jack', 25);
-  person2.showOutput();
-
-  var person3 = Person.guest();
-  person3.showOutput();
-}
-
+// ======================================================
 
 
 
