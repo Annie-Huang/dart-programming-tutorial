8884421000bcdccf58cd418c44f997064b6a0315
Annie Huang <annie.hn@gmail.com>
2020-01-11 18:26:21 +1100

    Define any property as a const using the final keyword

--- playground.dart
+++ playground.dart
@@ -566,7 +566,22 @@ multiple line of comment
 
 // ======================================================
 
+class X {
+  var name;
+//  final name; // type will be defined by inferred value
+  static const int age = 10;
 
+  X(this.name);
+}
+
+main() {
+  var x = X('jack');
+  print(x.name);
+
+  x.name = 'Jill';
+  print(x.name);
+
+}
 
 
 
